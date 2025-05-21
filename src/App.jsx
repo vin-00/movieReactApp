2fc6c8f61570248ed9757e445d4f571e7961c66d
vinay <vinaykhatri292@gmail.com>
2025-05-21 11:58:04 +0530

    fetch movie api call added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,66 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
 import Search from './components/Search';
+
+const API_BASE_URL='https://api.themoviedb.org/3'
+
+const API_KEY =  import.meta.env.VITE_TMDB_API_KEY
+
+const API_OPTIONS={
+  method:'GET',
+  headers:{
+    accept : 'application/json',
+    Authorization : `Bearer ${API_KEY}`
+  }
+}
 
 function App() {
 
   const [searchTerm , setSearchTerm] = useState("");
+
+  const[movieList , setMovieList] = useState([]);
+  const[errorMessage , setErrorMessage] = useState('');
+  const[isLoading, setIsLoading] = useState(false);
+
+  const fetchMovies = async()=>{
+    try{
+      const endpoint= `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+      
+      const response = await fetch(endpoint,API_OPTIONS)  
+
+      if(!response.ok){
+        throw new Error('Failed to fetch movies');
+      }
+
+      const data = await response.json();
+      if(data.Response=='False'){
+        setErrorMessage(data.Error || 'Failed to fetch movies')
+      }
+    }
+    catch(e){
+      console.error(e);
+      setErrorMessage("Error fetching movies. Please try again later.")
+    }
+  }
+  useEffect(()=>{
+    fetchMovies();
+  }, []) 
 
   return (
     <main>
       <div className='pattern' />
       <div className='wrapper' >
         <header>
-          <img src="" alt="Hero Banner" />
+          {/* <img src="hero." alt="Hero Banner" /> */}
           <h1>Find <span className='text-gradient' >Movies</span> You'll Enjoy without the Hassle</h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
 
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <section className='all-movies' >
+          <h2>All Movies</h2>
+          {errorMessage && <p className='text-red-500' >
+            {errorMessage}
+          </p> }
+        </section>
         
       </div>
     </main>
